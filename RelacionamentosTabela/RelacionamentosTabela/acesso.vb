7d62fffc97186965c7d0a070d197b08e5e19abd5
Hugo Dieb <hugodieb.hd@gmail.com>
2016-04-24 15:11:21 -0300

    commit pleo visual studio

--- RelacionamentosTabela/RelacionamentosTabela/acesso.vb
+++ RelacionamentosTabela/RelacionamentosTabela/acesso.vb
@@ -4,7 +4,7 @@ Imports System.Data.OleDb
 Module acesso
 
     Public Function getConnection() As OleDbConnection
-        Dim sql As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=G:\projetos_vb\RelacionamentosTabela\joinner.accdb"
+        Dim sql As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=G:\projetos_vb\CRUD\RelacionamentosTabela\joinner.accdb"
 
         Return New OleDbConnection(sql)
     End Function
